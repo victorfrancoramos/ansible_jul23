33b03fd0ec250fcd487624626ea6a76350494278
victorfrancoramos <formeitor@gmail.com>
2023-07-17 14:17:27 +0000

    Updated code

--- get_svm2.py
+++ get_svm2.py
@@ -3,6 +3,9 @@ from netapp_ontap.resources import Svm
 
 config.CONNECTION = HostConnection('‌192.168.0.102', username='admin', password='Netapp1!', verify=False)
 
-svm = Svm.get()
-
-print(svm.name)
+try:
+    for svm in Svm.get_collection(fields="uuid"):
+        svm.get()
+        print(svm.name)
+        print(svm.uuid)
+        #print("SVM name:-%s ; SVM uuid:-%s " % (svm.name, svm.uuid))
